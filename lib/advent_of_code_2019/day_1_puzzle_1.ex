c2d6346931792274dcce5a4c489b95177b651043
Kenny Evitt <kenny.evitt@gmail.com>
2019-12-04 01:00:40 -0500

    Add day 1, puzzle 1 input and solution

---
+++ lib/advent_of_code_2019/day_1_puzzle_1.ex
@@ -0,0 +1,22 @@
+defmodule AdventOfCode2019.Day1Puzzle1 do
+  def module_fuel_required(module_mass) do
+    div(module_mass, 3) - 2
+  end
+
+
+  def process_input() do
+    {:ok, input} = File.read("input/day-1-puzzle-1")
+
+    input
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(
+      0,
+      fn module_mass, running_total ->
+        running_total + module_fuel_required(module_mass)
+      end
+    )
+  end
+
+
+end
---
+++ test/advent_of_code_2019/day_1_puzzle_1_test.exs
@@ -0,0 +1,21 @@
+defmodule AdventOfCode2019.Day1Puzzle1Test do
+  use ExUnit.Case
+  import AdventOfCode2019.Day1Puzzle1
+  doctest AdventOfCode2019.Day1Puzzle1
+
+  test "example 1" do
+    assert module_fuel_required(12) == 2
+  end
+
+  test "example 2" do
+    assert module_fuel_required(14) == 2
+  end
+
+  test "example 3" do
+    assert module_fuel_required(1969) == 654
+  end
+
+  test "example 4" do
+    assert module_fuel_required(100756) == 33583
+  end
+end
