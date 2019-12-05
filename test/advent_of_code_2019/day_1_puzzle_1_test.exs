dab299e58a3931c7e2814be4b62cfb187b6bd866
Kenny Evitt <kenny.evitt@gmail.com>
2019-12-04 20:52:25 -0500

    Remove extraneous `doctest ...` statement from one test module

--- test/advent_of_code_2019/day_1_puzzle_1_test.exs
+++ test/advent_of_code_2019/day_1_puzzle_1_test.exs
@@ -1,7 +1,6 @@
 defmodule AdventOfCode2019.Day1Puzzle1Test do
   use ExUnit.Case
   import AdventOfCode2019.Day1Puzzle1
-  doctest AdventOfCode2019.Day1Puzzle1
 
   test "example 1" do
     assert module_fuel_required(12) == 2
