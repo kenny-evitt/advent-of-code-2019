0e1ef0e7b48520c8ed55e6871988dc31fa228e77
Kenny Evitt <kenny.evitt@gmail.com>
2019-12-17 00:48:23 -0500

    Add day 8, puzzle 1 input and solution

---
+++ lib/advent_of_code_2019/day_8_puzzle_1.ex
@@ -0,0 +1,52 @@
+defmodule AdventOfCode2019.Day8Puzzle1 do
+
+  @type color           :: 0..9
+  @type image_pixel     :: color
+  @type image_layer_row :: [image_pixel]
+  @type image_layer     :: [image_layer_row]
+  @type image           :: [image_layer]
+
+
+  @spec parse_image_data(String.t, pos_integer, pos_integer) :: image
+  def parse_image_data(image_data_string, width, height) do
+    image_data_string
+    |> String.trim_trailing() # Trim, e.g. newline, character(s)
+    |> String.split("", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(width * height)
+    |> Enum.map(fn layer -> Enum.chunk_every(layer, width) end)
+  end
+
+
+  @type flattened_layer :: [image_pixel]
+
+  @spec count_pixels(flattened_layer, color) :: non_neg_integer
+  def count_pixels(flattened_layer, color) do
+    Enum.count(flattened_layer, &(&1 == color))
+  end
+
+  @spec flattened_layer_with_fewest_pixels_by_color([flattened_layer], color) :: flattened_layer
+  def flattened_layer_with_fewest_pixels_by_color(flattened_layers, color) do
+    Enum.min_by(
+      flattened_layers,
+      &(count_pixels(&1, color))
+    )
+  end
+
+
+  @spec process_input() :: pos_integer
+  def process_input() do
+    {:ok, input} = File.read("input/day-8-puzzle-1")
+    image = parse_image_data(input, 25, 6)
+
+    flattened_layer_with_fewest_zero_pixels =
+      image
+      |> Enum.map(&List.flatten/1)
+      |> flattened_layer_with_fewest_pixels_by_color(0)
+
+    count_pixels(flattened_layer_with_fewest_zero_pixels, 1)
+    * count_pixels(flattened_layer_with_fewest_zero_pixels, 2)
+  end
+
+
+end
---
+++ test/advent_of_code_2019/day_8_puzzle_1_test.exs
@@ -0,0 +1,160 @@
+defmodule AdventOfCode2019.Day8Puzzle1Test do
+  use ExUnit.Case
+  use ExUnit.Parameterized
+  import AdventOfCode2019.Day8Puzzle1
+
+
+  test "parse image data" do
+    image_data_string = "123456789012"
+    width = 3
+    height = 2
+
+    expected_image =
+      [
+        [
+          [1, 2, 3],
+          [4, 5, 6]
+        ],
+        [
+          [7, 8, 9],
+          [0, 1, 2]
+        ]
+      ]
+
+    assert parse_image_data(image_data_string, width, height) == expected_image
+  end
+
+
+  test_with_params "count layer pixel colors",
+    fn layer, color, expected_count ->
+      flattened_layer = List.flatten(layer)
+      assert count_pixels(flattened_layer, color) == expected_count
+    end do
+      [
+        {
+          [
+            [1, 2, 3],
+            [4, 5, 6]
+          ],
+          0,
+          0
+        },
+        {
+          [
+            [1, 2, 3],
+            [4, 5, 6]
+          ],
+          1,
+          1
+        },
+        {
+          [
+            [1, 2, 3],
+            [4, 5, 6]
+          ],
+          2,
+          1
+        },
+        {
+          [
+            [7, 8, 9],
+            [0, 1, 2]
+          ],
+          0,
+          1
+        },
+        {
+          [
+            [7, 8, 9],
+            [0, 1, 2]
+          ],
+          1,
+          1
+        },
+        {
+          [
+            [7, 8, 9],
+            [0, 1, 2]
+          ],
+          2,
+          1
+        }
+      ]
+  end
+
+
+  test "find layer with fewest pixels by color example 1" do
+    flattened_layers =
+      [
+        [1, 2, 3, 4, 5, 6],
+        [7, 8, 9, 0, 1, 2]
+      ]
+
+    assert flattened_layer_with_fewest_pixels_by_color(flattened_layers, 0) == [1, 2, 3, 4, 5, 6]
+  end
+
+
+  test "find layer with fewest pixels by color example 2" do
+    flattened_layers =
+      [
+        [1, 2, 3, 4, 5, 6],
+        [7, 8, 9, 0, 1, 2]
+      ]
+
+    assert flattened_layer_with_fewest_pixels_by_color(flattened_layers, 3) == [7, 8, 9, 0, 1, 2]
+  end
+
+
+  test "find layer with fewest pixels by color example 3" do
+    flattened_layers =
+      [
+        [1, 2, 3, 4, 5, 6],
+        [7, 8, 9, 0, 1, 2]
+      ]
+
+    assert flattened_layer_with_fewest_pixels_by_color(flattened_layers, 7) == [1, 2, 3, 4, 5, 6]
+  end
+
+
+  # The following fails with an error like:
+  #
+  # ```
+  # ** (BadArityError) #Function<0.7843691/3 in AdventOfCode2019.Day8Puzzle1Test."test 'find layer with fewest pixels by color': number of 0"/1> with arity 3 called with 6 arguments (1, 2, 3, 4, 5, 6)
+  # ```
+  #
+  _ = """
+  test_with_params "find layer with fewest pixels by color",
+    fn flattened_layers, color, expected_flattened_layer ->
+      actual_flattened_layer = flattened_layer_with_fewest_pixels_by_color(flattened_layers, color)
+      assert actual_flattened_layer == expected_flattened_layer
+    end do
+      [
+        {
+          [
+            [1, 2, 3, 4, 5, 6],
+            [7, 8, 9, 0, 1, 2]
+          ],
+          0,
+          [1, 2, 3, 4, 5, 6]
+        },
+        {
+          [
+            [1, 2, 3, 4, 5, 6],
+            [7, 8, 9, 0, 1, 2]
+          ],
+          3,
+          [7, 8, 9, 0, 1, 2]
+        },
+        {
+          [
+            [1, 2, 3, 4, 5, 6],
+            [7, 8, 9, 0, 1, 2]
+          ],
+          7,
+          [1, 2, 3, 4, 5, 6]
+        },
+      ]
+  end
+  """
+
+end
